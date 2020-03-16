0c05f9a381c15e7074f776e228d4fdcd8c4ec2e4
Murad Malik <muradmalik7@gmail.com>
2020-03-17 00:53:01 +0500

    new changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,10 +10,6 @@ import TwitterIcon from '@material-ui/icons/Twitter';
 
 const Home = () => {
 
-    const showResume = () => {
-        window.location.replace('https://drive.google.com/file/d/1L-t8pJ6OGCnc3q12NtG8Hwj90Tp52cfQ/view?usp=sharing')
-    }
-
     return (
         <Grid container className="home" direction="row" justify="center">
             <Grid item xs={12} sm={12} md={6} lg={5} container direction="row" justify="center" alignItems="center">
@@ -23,7 +19,7 @@ const Home = () => {
                 <h2 className="name">Muhammad Murad Malik</h2>
                 <h3 className="degree">Software Developer</h3>
                 <h4 className="city">Islamabad, Pakistan</h4>
-                <Button onClick={showResume} variant="outlined" color="primary" size="medium" className="resume-btn">Resume</Button>
+                <a href="https://drive.google.com/open?id=1fsS2OgVfQHwFwB3fAuW9QkJSl5lkvsko" target="_blank" className="resume-btn">Resume</a>
                 <Box mt={2} className="box">
                     <a href="https://www.linkedin.com/in/murad-malik-13731ab6/"><LinkedInIcon className="social-icon" /></a>
                     <a href="https://github.com/muradmalik777"><GitHubIcon className="social-icon" /></a>
