069590e8b941703c6b31904f8606e706bbf2ed94
Murad Malik <muradmalik7@gmail.com>
2020-03-22 16:05:05 +0500

    new changes

--- src/helpers/Data.js
+++ src/helpers/Data.js
@@ -63,9 +63,27 @@ export const projects = () => {
     return [
         {
             name: "LatencyApp",
-            href:"",
-            stack: [],
-            role: ""
+            href:"http://latencyapp.com/",
+            role: "Frontend Developer",
+            stack: ["Html", "CSS", "Javascript", "MaterialUI", "ReactJS", "Redux", "Redux-Thunk", "Sockets", "FeatherJS", "Responsive Layout"],
+            fromDate: "November 2019",
+            toDate: "Present"
+        },
+        {
+            name: "Luminary",
+            href:"http://luminary.audio/",
+            role: "Frontend Developer",
+            stack: ["Html", "CSS", "Javascript", "ElementUI", "VueJS", "VueX", "Responsive Layout"],
+            fromDate: "August 2018",
+            toDate: "April 2019"
+        },
+        {
+            name: "Hireninja",
+            href:"http://hireninja.com/",
+            stack: ["Html", "CSS", "Javascript", "ElementUI", "VueJS", "VueX", "Responsive Layout"],
+            role: "Full Stack Developer",
+            fromDate: "June 2018",
+            toDate: "Dec 2019"
         }
     ]
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Grid, Box} from '@material-ui/core';
 import UserAvatar from '../components/UserAvatar/UserAvatar';
+import CodeIcon from '@material-ui/icons/Code';
+import {projects} from '../helpers/Data';
 
 const Projects = () => {
+    const [data] = useState(projects())
+
     return (
         <Grid container className="projects" direction="row" justify="center">
-            <Grid item xs={12} sm={11} md={11} lg={8} container direction="row" justify="center" alignItems="center">
+            <Grid item xs={12} sm={11} md={11} lg={12} container direction="column" justify="center" alignItems="center">
                 <UserAvatar />
-                <Box className="info" maxWidth="800px" p={3} display="flex" flexDirection="column" justify="center" alignItems="center">
-
+                <Box className="info" display="flex" flexWrap="wrap" flexDirection="row">
+                    {data && data.map((item, index) =>
+                        <Box key={index} className="project-box" display="flex" flexDirection="column" justifyContent="flex-start" alignItems="flex-start">
+                            <span className="company subtitle">{item.role}</span>
+                            <span className="position box-title">{item.name}</span>
+                            <span className="date small-title">{item.fromDate} - {item.toDate}</span>
+                            <Box className="stack-box" display="flex" flexDirection="row" flexWrap="wrap" justifyContent="flex-start" alignItems="center">
+                                {item.stack && item.stack.map((item, index) =>
+                                    <span key={index} className="chip"> {item} </span>
+                                )}
+                            </Box>
+                            <a className="link" target="_blank" href={item.href}>see project</a>
+                        </Box>
+                    )}
                 </Box>
             </Grid>
         </Grid>
