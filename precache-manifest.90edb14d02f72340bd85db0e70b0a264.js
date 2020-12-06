7b0b072b78296925b7c978c242a3bb14d400c18a
Murad Malik <muradmalik7@gmail.com>
2020-12-07 00:26:10 +0500

    Updates

---
+++ precache-manifest.90edb14d02f72340bd85db0e70b0a264.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fbd79c479d56fa1fc1003179b3846c2",
+    "url": "/murad/index.html"
+  },
+  {
+    "revision": "0da47e67599ca128a250",
+    "url": "/murad/static/css/main.dc40ac96.chunk.css"
+  },
+  {
+    "revision": "008b185f4bf49fcbb6e2",
+    "url": "/murad/static/js/2.2d416f51.chunk.js"
+  },
+  {
+    "revision": "0da47e67599ca128a250",
+    "url": "/murad/static/js/main.c782278f.chunk.js"
+  },
+  {
+    "revision": "62da952cb3b713626a56",
+    "url": "/murad/static/js/runtime-main.c541e917.js"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/murad/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/murad/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/murad/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/murad/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/murad/static/media/Roboto-Thin.321de678.ttf"
+  },
+  {
+    "revision": "6365705a9f6ab8f2dfff7b52ad454e1b",
+    "url": "/murad/static/media/murad.6365705a.jpg"
+  },
+  {
+    "revision": "5544aa385ecf2b848830e977d4c73785",
+    "url": "/murad/static/media/stack-overflow.5544aa38.svg"
+  }
+]);
